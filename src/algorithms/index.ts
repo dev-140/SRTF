fcc482e5097dba0f4994f66ff52ae2c2b3e3c77b
rox <roxmarzan14@gmail.com>
2024-04-24 19:44:51 +0800

    updates

--- src/algorithms/index.ts
+++ src/algorithms/index.ts
@@ -1,47 +1,38 @@
-import { fcfs } from './fcfs';
-import { sjf } from './sjf';
-import { srtf } from './srtf';
-import { rr } from './rr';
-import { npp } from './npp';
-import { pp } from './pp';
-import { AlgoType } from '../components/Input/AlgoSelect';
+// @ts-ignore
+import { fcfs } from "./fcfs";
+import { sjf } from "./sjf";
+import { srtf } from "./srtf";
+import { rr } from "./rr";
+import { npp } from "./npp";
+import { pp } from "./pp";
+import { AlgoType } from "../components/Input/AlgoSelect";
 
 export type ganttChartInfoType = {
-  job: string;
-  start: number;
-  stop: number;
+	job: string;
+	start: number;
+	stop: number;
 }[];
 
 export type solvedProcessesInfoType = {
-  job: string;
-  at: number;
-  bt: number;
-  ft: number;
-  tat: number;
-  wat: number;
+	job: string;
+	at: number;
+	bt: number;
+	ft: number;
+	tat: number;
+	wat: number;
 }[];
 
 export const solve = (
-  algo: AlgoType,
-  arrivalTime: number[],
-  burstTime: number[],
-  timeQuantum: number,
-  priorities: number[]
+	algo: AlgoType,
+	arrivalTime: number[],
+	burstTime: number[],
+	timeQuantum: number,
+	priorities: number[]
 ) => {
-  switch (algo) {
-    case 'FCFS':
-      return fcfs(arrivalTime, burstTime);
-    case 'SJF':
-      return sjf(arrivalTime, burstTime);
-    case 'SRTF':
-      return srtf(arrivalTime, burstTime);
-    case 'RR':
-      return rr(arrivalTime, burstTime, timeQuantum);
-    case 'NPP':
-      return npp(arrivalTime, burstTime, priorities);
-    case 'PP':
-      return pp(arrivalTime, burstTime, priorities);
-    default:
-      break;
-  }
+	switch (algo) {
+		case "SRTF":
+			return srtf(arrivalTime, burstTime);
+		default:
+			break;
+	}
 };
